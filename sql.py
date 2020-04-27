62408be006bca69d3a5481c5a80698027e8c07be
Antoinehrt <59895814+Antoinehrt@users.noreply.github.com>
2020-04-27 21:43:41 +0200

    Add files via upload
    Test SQL

--- sql.py
+++ sql.py
@@ -1,8 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect('inginious.sqlite')
+conn = sqlite3.connect('SQL\inginious.sqlite')
 
 cursor = conn.cursor()
 
-for row in cursor.execute("SELECT * from CLASSE"):
-    print(row)
+Abcisse = []
+Ordonnées = []
+for row in cursor.execute("SELECT DISTINCT(task) from user_tasks WHERE course LIKE 'LSINF1252'"):
+    Abcisse.append(row[0])
+
+
+for row2 in cursor.execute("SELECT COUNT(submission) from user_tasks WHERE task "):
+    Ordonnées.append(row2[0])
+print(Abcisse)
+print(len(Ordonnées))
