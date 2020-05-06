dc680fded82a19ee53af625f13e0f515790e1625
Antoinehrt <59895814+Antoinehrt@users.noreply.github.com>
2020-05-06 08:48:45 +0200

    Add files via upload
    Nouvelles fonction, nouveau graph

---
+++ SQL.py
@@ -0,0 +1,132 @@
+import sqlite3
+
+conn = sqlite3.connect('SQL\inginious.sqlite')
+
+cursor = conn.cursor()
+
+
+def course_task_submission(course):
+    abscissa = []
+    ordinate = []
+    select = "SELECT DISTINCT(task) from user_tasks WHERE course LIKE '{}'".format(course)
+
+    for row in cursor.execute(select):
+        abscissa.append(row[0])
+
+    for i in abscissa:
+        select2 = "SELECT COUNT(submission) from user_tasks WHERE task LIKE '{}'".format(i)
+        for row2 in cursor.execute(select2):
+            ordinate.append(row2[0])
+
+    return abscissa, ordinate
+
+
+def student_task_grade(student):
+    result = []
+    x = 0
+    y = 0
+    select3 = "SELECT grade from user_tasks WHERE username like '{}'".format(student)
+    for row3 in cursor.execute(select3):
+        if row3 == (100.0,):
+            x += 1
+        else:
+            y += 1
+    result.append(x)
+    result.append(y)
+
+    return result
+
+
+def moyenne_grade_task(x):  # Bastien
+    liste = []
+    liste_task = []
+    liste_moyenne = []
+    select = "SELECT task, grade from submissions WHERE course LIKE '{}'".format(x)
+    for row in cursor.execute(select):
+
+        if row[0] not in (i[0] for i in liste):
+            liste.append([row[0], row[1], 1])
+        else:
+            count = -1
+            for z in (i[0] for i in liste):
+                count += 1
+                if z == row[0]:
+                    liste[count][1] = float(liste[count][1]) + float(row[1])
+                    liste[count][2] = float(liste[count][2]) + 1.0
+
+    for y in liste:
+        liste_moyenne.append(y[1] / y[2])
+        liste_task.append(y[0])
+    del liste
+    if len(liste_moyenne) == len(liste_task):
+        return liste_moyenne, liste_task
+
+
+def rst_or_html(
+        course):  # graphique circulaire qui permet de voir si les cours en question a des soumissions html ou rst
+    result = []
+    x = 0
+    y = 0
+    select3 = "SELECT response_type FROM submissions WHERE course LIKE'{}'".format(course)
+    for row3 in cursor.execute(select3):
+        if row3[0] == 'rst':
+            x += 1
+        else:
+            y += 1
+    result.append(x)
+    result.append(y)
+
+    return result
+
+
+def student_point_for_task(
+        student):  # graphique qui montre les points d'un etudiant pour chaque task (le cours etant implicite)
+    task = []
+    point = []
+
+    select = "SELECT DISTINCT(task) from user_tasks WHERE username LIKE '{}'".format(student)
+
+    for row in cursor.execute(select):
+        task.append(row[0])
+
+    for i in task:
+        select2 = "SELECT MAX(grade) from user_tasks WHERE task LIKE '{}' AND username LIKE '{}'".format(i, student)
+        for row2 in cursor.execute(select2):
+            point.append(row2[0])
+
+    return task, point
+
+
+def student_tries_for_task(
+        student):  # graphique qui montre le nombre de tentative d'un etudiant pour chaque task (le cours etant implicite)
+    task = []
+    tries = []
+
+    select = "SELECT DISTINCT(task) from user_tasks WHERE username LIKE '{}'".format(student)
+
+    for row in cursor.execute(select):
+        task.append(row[0])
+
+    for i in task:
+        select2 = "SELECT tried from user_tasks WHERE task LIKE '{}' AND username LIKE '{}'".format(i, student)
+        for row2 in cursor.execute(select2):
+            tries.append(row2[0])
+
+    return task, tries
+
+
+def suceeded_task(
+        task):  # graphique circulaire qui permet de voir le pourcentage de bonnes reponses sur le cours en question
+    result = []
+    x = 0
+    y = 0
+    select3 = "SELECT succeeded FROM user_tasks WHERE task LIKE'{}'".format(task)
+    for row3 in cursor.execute(select3):
+        if row3[0] == 'true':
+            x += 1
+        else:
+            y += 1
+    result.append(x)
+    result.append(y)
+
+    return result
--- app.py
+++ app.py
@@ -1,21 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from SQL import student_task_grade, student_point_for_task
 
 app = Flask(__name__)
 
+name_student = "anon_1317"
+
+sql = student_task_grade(name_student)
+
+
+studentgraph1 = student_point_for_task(name_student)
+
+X = studentgraph1[0]
+y = studentgraph1[1]
 
 @app.route('/')
 def Homepage():
     return render_template('base.html')
 
 
-@app.route('/course-graph')
+@app.route('/course')
 def course_graph():
-    return render_template('course_graph.html')
+    return render_template('course-selection.html')
 
 
 @app.route('/student-graph')
 def student_graph():
-    return render_template('student_graph.html')
+    return render_template('student-graph.html', name=name_student)
+
+
+@app.route('/course-LINFO1252')
+def LINFO1252_graph():
+    return render_template('course/LINFO1252.html')
+
+
+@app.route('/course-LINFO1012')
+def LINFO1012_graph():
+    return render_template('course/LINFO1012.html')
+
+
+@app.route('/course-LESPO1412')
+def LESPO1412_graph():
+    return render_template('course/LESPO1412.html')
+
+
+@app.route('/data')
+def data():
+    return jsonify({'results': sql},{'X': X})
 
 
 if __name__ == '__main__':
