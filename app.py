5f2ebe77cf7fe1b4f0e24d2c1ad899d8dc0d9898
Antoinehrt <59895814+Antoinehrt@users.noreply.github.com>
2020-05-13 09:09:48 +0200

    Add files via upload
    Corrections de beug; ajouts de commentaires; màj page task

--- app.py
+++ app.py
@@ -1,41 +1,52 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from SQL import student_task_grade, student_point_for_task, moyenne_grade_task, course_task_submission, rst_or_html, \
     suceeded_task
 
+# Fichier générant les pages HTML
+
 app = Flask(__name__)
-
-name_student = "anon_1317"
-
-sql = student_task_grade(name_student)
-
-studentgraph1 = student_point_for_task(name_student)
-
-X = studentgraph1[0]
-y = studentgraph1[1]
 
 
 @app.route('/')
 def Homepage():
+    """
+    :return: Page d'acceuil du site web
+    """
     return render_template('base.html')
 
 
 @app.route('/course')
 def course_graph():
+    """
+    :return: page de selection de cours du site web
+    """
     return render_template('course-selection.html')
 
 
 @app.route('/student-graph')
 def student_graph():
+    """
+    pre: input venu du code html
+    :return: page des graphiques sur les données par étudiants
+    """
     # TODO Rajouter les inputs
     return render_template('student-graph.html', name=name_student)
 
+
 @app.route('/en-savoir-plus')
 def en_savoir_plus():
+    """
+    :return: page contenant des informations sur notre site et sur notre code.
+    """
     return render_template('en-savoir-plus.html')
 
 
 @app.route('/course-LINFO1252')
 def LINFO1252_graph():
+    """
+    pre: appel de fonction provenant du fichier SQL.py. Les données serve de valeur pour les graphiques.
+    :return: page du cours contenant 3 graphiques
+    """
     values_course = course_task_submission("LSINF1252")[1]
     labels_course = course_task_submission("LSINF1252")[0]
     values2_course = rst_or_html("LSINF1252")
@@ -47,6 +58,10 @@ def LINFO1252_graph():
 
 @app.route('/course-LINFO1012')
 def LINFO1012_graph():
+    """
+    pre: appel de fonction provenant du fichier SQL.py. Les données serve de valeur pour les graphiques.
+    :return: page du cours contenant 3 graphiques
+    """
     values_course = course_task_submission("LEPL1402")[1]
     labels_course = course_task_submission("LEPL1402")[0]
     values2_course = rst_or_html("LEPL1402")
@@ -57,7 +72,11 @@ def LINFO1012_graph():
 
 
 @app.route('/course-LESPO1412')
-def LESPO1412_graph():
+def LEPL1402_graph():
+    """
+    pre: appel de fonction provenant du fichier SQL.py. Les données serve de valeur pour les graphiques.
+    :return: page du cours contenant 3 graphiques
+    """
     values_course = course_task_submission("LSINF1101-PYTHON")[1]
     labels_course = course_task_submission("LSINF1101-PYTHON")[0]
     values2_course = rst_or_html("LSINF1101-PYTHON")
@@ -70,10 +89,12 @@ def LESPO1412_graph():
 
 @app.route('/tasks-graph')
 def tasks():
+    """
+    pre: input venant du code html
+    :return: page contenant des graphs avec des informations en fonction des tasks
+    """
     values4_course = suceeded_task('BinarySearch')  # TODO Rajouter les inputs
     return render_template('tasks.html', values4=values4_course)
-
-
 
 
 if __name__ == '__main__':
